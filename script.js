7d8865fa4ae0ba6de7057089d2c6c685860416d2
naiadidub <antonio.zaghi1405@gmail.com>
2022-04-22 16:00:26 +0200

    start

---
+++ script.js
@@ -0,0 +1,33 @@
+const todolist = new Vue({
+    el: '#app',
+    data: {
+        dolist: [
+            {
+                toDo: 'one hour of workout',
+                check: false,
+            },
+            {
+                toDo: 'clean the bathroom',
+                check: false,
+            },
+            {
+                toDo: 'make some dubstep',
+                check: false,
+            },
+            {
+                toDo: 'having fun with vue',
+                check: false,
+            },
+            {
+                toDo: 'watch the sky',
+                check: false,
+            },
+            {
+                toDo: 'sleep on a cloud',
+                check: false,
+            },
+        ]
+    },
+    methods: {}
+
+})
